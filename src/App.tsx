c719874e83d1e7ebac073913fb05c11103e42cf5
Irakli <koregm@gmail.com>
2022-02-28 16:03:57 +0400

    Optional Field in Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import React from 'react';
-
+import React from "react";
 
 // Basic Types in Typescript
 
-let name:  String
-let age: number
-let isStudent: boolean
+let name: String;
+let age: number;
+let isStudent: boolean;
 // array of string
-let hobbies: string[]
+let hobbies: string[];
 // array of one number and one string
-let role: [number, string]
-
+let role: [number, string];
 
 // define object
 type Person = {
-  name: string,
-  age: number
-}
+  name: string;
+  age?: number;
+};
 
 let person: Person = {
   name: "John",
-  age: 2
-}
+};
 
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+  return <div className="App">Hello world</div>;
 }
 
 export default App;
