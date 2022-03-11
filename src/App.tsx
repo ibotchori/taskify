d3692a5943b19a5c81e6f6ddb56fe6c73bddd06c
Irakli <koregm@gmail.com>
2022-03-11 11:46:24 +0400

    Functional Component type

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,7 @@
 import React from "react";
 
-// Basic Types in Typescript
-
-let name: String;
-// Union Type
-let age: number | string;
-age = "test";
-
-let isStudent: boolean;
-// array of string
-let hobbies: string[];
-// array of one number and one string
-let role: [number, string];
-
-// function type, receives string and returns number
-let printName: (name: string) => number;
-
-// define object
-type Person = {
-  name: string;
-  age?: number;
-};
-let person: Person = {
-  name: "John",
-};
-
-// Array of Person Object
-let lotsOfPeople: Person[];
-
-// any type, not recommended
-let job: any;
-// unknown type
-let personName: unknown;
-
-function App() {
+const App: React.FC = () => {
   return <div className="App">Hello world</div>;
-}
+};
 
 export default App;
