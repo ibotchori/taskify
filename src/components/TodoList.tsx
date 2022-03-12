e7c3873224490c401ea176c91a4279ba097e69b3
Irakli <koregm@gmail.com>
2022-03-12 22:30:35 +0400

    Building App UI for Drag and Drop

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,15 +9,31 @@ interface props {
 
 const TodoList: React.FC<props> = ({ todos, setTodos }) => {
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <SingleTodo
-          key={todo.id}
-          todo={todo}
-          todos={todos}
-          setTodos={setTodos}
-        />
-      ))}
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">Active Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+
+      <div className="todos remove">
+        {" "}
+        <span className="todos__heading">Completed Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
     </div>
   );
 };
